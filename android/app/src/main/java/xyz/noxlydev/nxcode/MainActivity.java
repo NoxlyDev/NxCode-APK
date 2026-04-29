8913b3fa517c671065427e65da416d11176898b2
NoxlyDev <knoxzyv@gmail.com>
2026-04-29 20:38:59 +0800

    fixed copy paste termminal

--- android/app/src/main/java/xyz/noxlydev/nxcode/MainActivity.java
+++ android/app/src/main/java/xyz/noxlydev/nxcode/MainActivity.java
@@ -1,5 +1,20 @@
 package xyz.noxlydev.nxcode;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        WebView webView = getBridge().getWebView();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setOnLongClickListener(v -> true); // prevent default long press
+    }
+}
